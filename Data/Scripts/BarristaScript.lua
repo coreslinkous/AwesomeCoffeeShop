490ec480caa1b88d82798251dd95cae36d63f42e
slinkous <slinkous@manticoregames.com>
2020-10-28 11:41:28 -0700

    Add more scripts

---
+++ Data/Scripts/BarristaScript.lua
@@ -0,0 +1,13 @@
+﻿local trigger = script:GetCustomProperty("Trigger"):WaitForObject()
+local barrista = script:GetCustomProperty("Barrista"):WaitForObject()
+
+
+
+function OnBeginOverlap(whichTrigger, other)
+	if other:IsA("Player") then
+		barrista:PlayAnimation("unarmed_wave")
+	end
+end
+
+
+trigger.beginOverlapEvent:Connect(OnBeginOverlap)
